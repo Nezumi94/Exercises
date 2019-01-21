dc0a19887bb6824534dba0f94cb3a1708b7e34bf
Nezumi94 <38926839+Nezumi94@users.noreply.github.com>
2019-01-21 12:34:20 +0100

    Add files via upload

---
+++ Ex1.py
@@ -0,0 +1,24 @@
+#W tym zadaniu liczymy pole koła i obwód okręgu
+
+WartoscPi = 3.14
+PromienOkregu = 5
+PoleKola =  WartoscPi * PromienOkregu **2
+print('Pole Koła wynosi:',PoleKola)
+ObwodOkregu = 2 * WartoscPi * PromienOkregu
+print('Obwód okręgu wynosi:',ObwodOkregu)
+
+#Pole Prostokąta
+
+BokA = 5
+BokB = 7
+PoleProstokata = BokA * BokB
+print('Pole prostokąta wynosi:',PoleProstokata)
+
+#Pole trapezu
+
+PodstawaA = 10
+PodstawaB = 6
+Wysokosc = 4
+PoleTrapezu = (PodstawaA + PodstawaB) * Wysokosc // 2
+print('Pole Trapezu to:',PoleTrapezu)
+
